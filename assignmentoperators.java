c43dd3e4296f54571958ee97f3ef707c206e1457
sree11920 <kasireddygarihemasree@gmail.com>
2024-12-03 10:56:36 +0530

    Add files via upload

---
+++ assignmentoperators.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class assignmentoperators{
+    public static void main(String args[]){
+        //accepting a string
+        int a=45;
+        System.out.println(a+=46);
+        System.out.println(a-=9);
+        System.out.println(a*=2);
+    }
+}
