d5e803b47b828be431eb1ca6cb1b6173b35f2fb7
23hr1a0455 <kasireddygarihemasree@gmail.com>
2024-12-03 10:30:25 +0530

    Add files via upload

---
+++ airthematicoperators.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class airthematicoperators{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }    
+}        
+
+        
+        
+
+        
+
+
+    
