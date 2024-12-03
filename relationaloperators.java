1889ad25962163a7b1acb4ee1ba17961ce215993
23hr1a0455 <kasireddygarihemasree@gmail.com>
2024-12-03 10:30:58 +0530

    Add files via upload

---
+++ relationaloperators.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class relationaloperators{
+    public static void main(String args[]){
+        //accepinting a string
+        int a=23;
+        int b=34;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
